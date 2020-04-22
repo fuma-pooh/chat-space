4a1d49ac95522f4b4e0b47bba7a1e50c97a4a886
fuma-pooh <fuma.kido@di-v.co.jp>
2020-04-22 14:18:31 +0900

    非同期完了

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+     function buildHTML(message){
+       if ( message.image ) {
+       var html =
+         `<div class="chat-main__messages__message__info">
+            <div class="chat-main__messages__message__info__talker">
+              ${message.user_name}
+            </div>
+            <div class="chat-main__messages__message__info__talker__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="chat-main__messages__message">
+            <p class="chat-main__messages__message__text">
+              ${message.content}
+            </p>
+            <div class="lower-message__image">
+              <img src=${message.image} >
+            </div>
+          </div>`
+          return html;
+        } else {
+          var html =
+           `<div class="chat-main__messages__message">
+              <div class="chat-main__messages__message__info">
+                <div class="chat-main__messages__message__info__talker">
+                  ${message.user_name}
+                </div>
+                  <div class="chat-main__messages__message__info__talker__date">
+                    ${message.created_at}
+                  </div>
+              </div>
+            <div class="chat-main__messages__message">
+              <p class="chat-main__messages__message__text">
+                ${message.content}
+              </p>
+            </div>
+            </div>`
+         return html;
+        };
+     }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.chat-main__messages').append(html);
+       $('form')[0].reset();
+       $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+     })
+     .always(function() {
+       $('.chat-main__form__new-message__send__button').prop('disabled', false);
+     })
+     .fail(function() {
+      alert("メッセージ送信に失敗しました");
+     });
+  });
+});
--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
