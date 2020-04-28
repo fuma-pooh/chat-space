e57462071669259513009b698e2e6ec0fb0b87b3
fuma-pooh <fuma.kido@di-v.co.jp>
2020-04-28 15:04:14 +0900

    自動更新２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,7 @@ $(function(){
       return html;
     };
   }
-$('#new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
   var url = $(this).attr('action');
@@ -64,29 +64,22 @@ $('#new_message').on('submit', function(e){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-});
+  });
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.chat-main__messages__message:last').data("message-id");
     console.log(last_message_id)
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-      //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.chat-main__messages').append(insertHTML);
         $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
       }
